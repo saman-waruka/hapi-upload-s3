a7dc46916415c7e5d9e625d6005ae741374ba3a2
saman <saman_waruka@cmu.ac.th>
2020-03-02 18:49:01 +0700

    can upload to s3 now

--- src/file-upload.js
+++ src/file-upload.js
@@ -1,39 +1,46 @@
-require('dotenv').config();
+require("dotenv").config();
 
-const aws = require('aws-sdk'); // import aws-sdk
-// Setting aws configurations 
+const aws = require("aws-sdk"); // import aws-sdk
+// Setting aws configurations
 aws.config.update({
-    secretAccessKey: process.env.SECRET_ACCESS_KEY,
-    accessKeyId: process.env.ACCESS_KEY_ID,
-    region: process.env.REGION
+  secretAccessKey: process.env.SECRET_ACCESS_KEY,
+  accessKeyId: process.env.ACCESS_KEY_ID,
+  region: process.env.REGION
 });
-console.log( " Env \n secretAccessKey",process.env.SECRET_ACCESS_KEY);
-console.log( "accessKeyId",process.env.ACCESS_KEY_ID);
-console.log( "REGION",process.env.REGION);
-console.log( "bucketName",process.env.BUCKET_NAME);
+console.log(" Env \nsecretAccessKey", process.env.SECRET_ACCESS_KEY);
+console.log("accessKeyId", process.env.ACCESS_KEY_ID);
+console.log("REGION", process.env.REGION);
+console.log("bucketName", process.env.BUCKET_NAME);
 // Decent bucket name
 const bucketName = process.env.BUCKET_NAME;
 // Initiating S3 instance
 const s3 = new aws.S3({
-    apiVersion: '2006-03-01',
-    params: {Bucket: bucketName}
+  apiVersion: "2006-03-01",
+  //   params: { Bucket: bucketName }
+  accessKeyId: process.env.ACCESS_KEY_ID,
+  secretAccessKey: process.env.SECRET_ACCESS_KEY
 });
 // Options you can choose to set to accept files upto certain size limit
-const options = {partSize: 10 * 1024 * 1024, queueSize: 1};
+const options = { partSize: 10 * 1024 * 1024, queueSize: 1 };
 // The heart
-// "Key" accepts name of the file, keeping it a timestamp for simplicity 
+// "Key" accepts name of the file, keeping it a timestamp for simplicity
 // "Body" accepts the file
 
-const filedata = "mockdata";
-
-
-async function upload(file){
-  console.log(" Upload called")
-   const params = {Bucket: bucketName, Key:     `${process.env.BUCKET_PATH_KEY}${Date.now().toString()}`, Body: file};
-    let fileResp = null;
-    await s3.upload(params, options).promise().then((res) => {
-        fileResp = res;
+async function upload(file, name) {
+  console.log(" Upload called");
+  const params = {
+    Bucket: bucketName,
+    // Key: `${process.env.BUCKET_KEY_PREFIX}${Date.now().toString()}_${name}`,
+    Key: `${process.env.BUCKET_KEY_PREFIX}${name}`,
+    Body: file
+  };
+  let fileResp = null;
+  await s3
+    .upload(params, options)
+    .promise()
+    .then(res => {
+      fileResp = res;
     });
-    return fileResp;
-};
+  return fileResp;
+}
 module.exports = upload;
--- src/index.js
+++ src/index.js
@@ -1,72 +1,77 @@
-'use strict';
+"use strict";
 
-const Hapi = require('@hapi/hapi');
+const Hapi = require("@hapi/hapi");
 
 const upload = require("./file-upload");
 
-
-
-
-
 const init = async () => {
+  const server = Hapi.server({
+    port: 4000,
+    host: "localhost",
+    routes: {
+      cors: true
+    }
+  });
 
-    const server = Hapi.server({
-        port: 3000,
-        host: 'localhost',
-        routes: {
-            cors: true
-        }
-    });
+  server.route({
+    method: "GET",
+    path: "/",
+    handler: (request, h) => {
+      console.log(" Hell");
+      return "Hello World!";
+    }
+  });
 
-    // server.connection({ routes: { cors: true } })
+  server.route({
+    method: "POST",
+    path: "/upload",
+    options: {
+      handler: async (request, h) => {
+        // console.log(" Upload Called", request);
 
-    server.route({
-        method: 'GET',
-        path: '/',
-        handler: (request, h) => {
+        let responseFile = null;
+        await upload(request.payload.file, request.payload.name)
+          .then(resp => {
+            console.log(" Respons ====> ", resp);
+            responseFile = { fileUrl: resp.Location };
+          })
+          .catch(err => {
+            responseFile = err.message;
+          });
+        return responseFile;
+      },
+      payload: {
+        // output: "stream",
+        // parse: true,
+        // allow: "multipart/form-data"
+        // maxBytes: 1024 * 1024 * 100,
+        // timeout: false
+        output: "stream",
+        parse: true,
+        multipart: true
+      }
+    }
+  });
 
-            return 'Hello World!';
-        }
-    });
+  server.events.on("response", function(request) {
+    console.log(
+      request.info.remoteAddress +
+        ": " +
+        request.method.toUpperCase() +
+        " " +
+        request.path +
+        " --> " +
+        request.response.statusCode
+    );
+  });
 
-    server.route({
-        method: 'POST',
-        path: '/upload',
-        config: {
-            handler: async (request, h) => {
-                console.log( " Upload Called");
-    
-                let responseFile = null;
-                await upload(request.payload.file).then((resp) => {
-    
-                    responseFile = {fileUrl: resp.Location};
-                }).catch((err) => {
-                    responseFile = err.message;
-                });
-                return responseFile;
-            },
-            payload: {
-                output: 'stream',
-                parse: true,
-                allow: 'multipart/form-data',
-                maxBytes: 1024 * 1024 * 100,
-                timeout: false,
-            }
-        }
-    });
-    
-    server.events.on('response', function (request) {
-        console.log(request.info.remoteAddress + ': ' + request.method.toUpperCase() + ' ' + request.path + ' --> ' + request.response.statusCode);
-    });
-
-    await server.start();
-    console.log('Server running on %s', server.info.uri);
+  await server.start();
+  console.log("Server running on %s", server.info.uri);
 };
 
-process.on('unhandledRejection', (err) => {
-
-    console.log(err);
-    process.exit(1);
+process.on("unhandledRejection", err => {
+  console.log(err);
+  process.exit(1);
 });
 
 init();
